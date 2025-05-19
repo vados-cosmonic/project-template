9b2375741e28920b6858dd955645104aaaf3449f
Victor Adossi <vadossi@cosmonic.com>
2025-05-20 03:37:25 +0900

    refactor: more simple tests
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- js/packages/example-js/test/test.js
+++ js/packages/example-js/test/test.js
@@ -8,5 +8,8 @@ const symbolDispose = Symbol.dispose || Symbol.for("dispose");
 suite("basic tests", () => {
   test("named", async () => {
     assert.strictEqual(greet("joe"), "Hello, joe");
+    assert.strictEqual(greet(), "Hello, world");
+    assert.strictEqual(greet(undefined), "Hello, world");
+    assert.strictEqual(greet(null), "Hello, world");
   });
 });
